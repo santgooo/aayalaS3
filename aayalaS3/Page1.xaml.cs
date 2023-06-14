0e3ba983b8f45a395d495370a9afca6d0d566f1c
master <master@SMLAB108PC10_23>
2023-06-13 21:21:36 -0500

    Agregar archivos de proyecto.

---
+++ aayalaS3/Page1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace aayalaS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string dato1 = txtDato1.Text;
+            double dato2 = Convert.ToDouble(txtDato2.Text);
+
+            Navigation.PushAsync(new Page2(dato1, dato2));
+        }
+    }
+}
---
+++ aayalaS3/Page2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace aayalaS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string dato1, double dato2)
+        {
+            InitializeComponent();
+            lblDato1.Text = "Dato Uno ==>" + dato1;
+            txtDato2.Text = "Dato Uno ==>" + dato2.ToString();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
